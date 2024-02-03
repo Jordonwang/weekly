86bb3e04b89f7a46351cfbfd89c95a3f8af5a400
Jordonwang <jordonwang@qq.com>
2024-02-03 20:52:11 +0800

    update build.py

--- build.py
+++ build.py
@@ -25,14 +25,9 @@ if __name__ == "__main__":
         url   = 'https://weekly.Jordonwang.fun/posts/' + oldTitle
         title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
         readmeMd= '* [{}]({})\n'.format(title, url)
-        dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
         num = int(oldTitle.split('-')[0])
         if index < 5 :
-          if num < 100 :
-            modified = dateList[99-num]
-          else :
-            modified = fetch_ci_time('/src/pages/posts/' + filepath)
-
+          modified = fetch_ci_time('/src/pages/posts/' + filepath)
           recentMd= '* [{}]({}) - {}\n'.format(title, url, modified)
           recentfile.write(recentMd)
         readmefile.write(readmeMd)
